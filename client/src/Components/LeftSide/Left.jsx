a86bb6e57d0c7fce5888790cece741a9e26782ef
douglas86 <douglasmaxton@gmail.com>
2021-03-08 21:32:20 +0200

    Created two columns of radio btn's

--- client/src/Components/LeftSide/Left.jsx
+++ client/src/Components/LeftSide/Left.jsx
@@ -1,34 +1,42 @@
 import React from "react";
 import useStyles from "./Styles";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Left = () => {
   const classes = useStyles();
-  let lab = [
-    "e books",
-    "Movies",
-    "podcasts",
-    "music",
-    "audiobook",
-    "short film",
-    "TV shows",
-    "software",
-    "all",
-  ];
+  let labelLeft = ["e books", "Movies", "podcasts", "music"];
+  let labelRight = ["audiobook", "short film", "TV shows", "software"];
   return (
     <div>
-      <h1 className={classes.left}>Itunes search API</h1>
-      <input
-        className={classes.input}
-        type="text"
-        placeholder="Search"
-        required
-      />
-      {lab.map((i) => (
-        <div>
-          <input type="radio" name="rad" className={classes.radio} />
-          <label>{i}</label>
-        </div>
-      ))}
+      <Container>
+        <h1 className={classes.left}>Itunes search API</h1>
+        <input
+          className={classes.input}
+          type="text"
+          placeholder="Search"
+          required
+        />
+        <Row>
+          <Col xl={6}>
+            {labelLeft.map((l, i) => (
+              <div key={l}>
+                <input type="radio" name="rad" className={classes.radio} />
+                <label>{l}</label>
+              </div>
+            ))}
+          </Col>
+          <Col xl={6}>
+            {labelRight.map((l, i) => (
+              <div key={l}>
+                <input type="radio" name="rad" className={classes.radio} />
+                <label>{l}</label>
+              </div>
+            ))}
+          </Col>
+        </Row>
+        <input type="radio" className={classes.radio} name="rad" />
+        <label>all</label>
+      </Container>
     </div>
   );
 };
